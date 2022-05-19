15b20cab2680bf08281b4c7f0dbf5998bdc7e737
SergeyBog <serg123bb@gmail.com>
2022-05-19 13:57:39 +0300

    Add buttons to control movement on GUI

--- Quoridors/src/main/java/com/quoridors/Quoridors/input/impl/CommandInput.java
+++ Quoridors/src/main/java/com/quoridors/Quoridors/input/impl/CommandInput.java
@@ -34,54 +34,58 @@ public class CommandInput implements Input {
 
 	@Override
 	public void waitForCommand() {
-		Scanner scanner = new Scanner(System.in);
-		do {
-			System.out.println("Input your command");
-			String[] command = scanner.nextLine().split(" ");
-			String param1 = null;
-			String param2 = null;
-			String input = command[0];
-			if (command.length > 1 && command.length < 4) {
-				param1 = command[1];
-				param2 = command[2];
-			}
-			switch (input) {
-			case "up":
-				game.changePosition(-2, 0);
-				break;
-			case "down":
-				game.changePosition(+2, 0);
-				break;
-			case "right":
-				game.changePosition(0, +2);
-				break;
-			case "left":
-				game.changePosition(0, -2);
-				break;
-			case "diagRU":
-				game.changePosition(-2, 2);
-				break;
-			case "diagLU":
-				game.changePosition(-2, -2);
-				break;
-			case "diagRD":
-				game.changePosition(2, 2);
-				break;
-			case "diagLD":
-				game.changePosition(2, -2);
-				break;
-			case "wall":
-				if (command.length > 1) {
-					game.placeWall(Integer.parseInt(param1), Integer.parseInt(param2));
+		if (!game.getIsABot() && !game.getIsBotMoving()) {
+			Scanner scanner = new Scanner(System.in);
+			do {
+				System.out.println("Input your command");
+				String[] command = scanner.nextLine().split(" ");
+				String param1 = null;
+				String param2 = null;
+				String input = command[0];
+				if (command.length > 1 && command.length < 4) {
+					param1 = command[1];
+					param2 = command[2];
 				}
-				break;
-			default:
-				System.out.println("Invalid command");
-				break;
-			}
+				switch (input) {
+				case "up":
+					game.movePlayer(-2, 0);
+					break;
+				case "down":
+					game.movePlayer(+2, 0);
+					break;
+				case "right":
+					game.movePlayer(0, +2);
+					break;
+				case "left":
+					game.movePlayer(0, -2);
+					break;
+				case "diagRU":
+					game.movePlayer(-2, 2);
+					break;
+				case "diagLU":
+					game.movePlayer(-2, -2);
+					break;
+				case "diagRD":
+					game.movePlayer(2, 2);
+					break;
+				case "diagLD":
+					game.movePlayer(2, -2);
+					break;
+				case "wall":
+					if (command.length > 1) {
+						game.placeWall(Integer.parseInt(param1), Integer.parseInt(param2));
+					}
+					break;
+				default:
+					System.out.println("Invalid command");
+					break;
+				}
 
-		} while (!game.gameFinished());
-		scanner.close();
+			} while (!game.gameFinished());
+			scanner.close();
+		}else {
+			game.decideBotMovement();
+		}
 	}
 
 }
